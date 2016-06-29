86c6ba9c5bc5e855ea94c15f027d38e8e9e52f81
varunk1392 <varunkulkarni2017@u.northwestern.edu>
2016-06-29 16:20:32 -0500

    Assignment 1 and 2

---
+++ DebugProject/DebugProject/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DebugProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader ("StudentGrades.txt"))
+            {
+                string line;
+                List<StudentGrade> gradeList = new List<StudentGrade> ();
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] row = line.Split(',');
+
+                    StudentGrade grade = new StudentGrade ();
+
+                    grade.StudentName = row[0];
+                    try
+                    {
+                        grade.PointsEarned = int.Parse(row[1]);
+                    }
+                    catch
+                    {
+                        System.Console.WriteLine("Failure in parsing" + row[1] + "to an integer value");
+                    }
+
+                    try
+                    {
+                        grade.PointsPossible = int.Parse(row[2]);
+                    }
+                    catch
+                    {
+                        System.Console.WriteLine("Failure in parsing" + row[2] + "to an integer value of 100");
+                    }
+                    
+
+                    gradeList.Add(grade);
+                }
+
+                OutputGrades(gradeList);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void OutputGrades(List<StudentGrade> grades)
+        {
+            Console.WriteLine("Name".PadRight(20, ' ') + "Pts Earned".PadRight(20, ' ') + "Pts Possible".PadRight(20, ' ') + "Average".PadRight(10, ' ') + "Ltr Grade");
+
+            for (var x = 0; x < grades.Count; x++ )
+            {
+                StudentGrade grade = grades[x];
+
+                grade.CalculateAverage();
+                grade.DetermineLetterGrade();
+
+                Console.WriteLine(grade.StudentName.PadRight(20, ' ') + grade.PointsEarned.ToString().PadRight(20, ' ') + grade.PointsPossible.ToString().PadRight(20, ' ') + grade.Average.ToString().PadRight(10, ' ') + grade.LetterGrade);
+            }
+        }
+    }
+}
---
+++ DebugProject/DebugProject/StudentGrade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugProject
+{
+    public class StudentGrade
+    {
+        public string StudentName { get; set; }
+        public decimal PointsEarned { get; set; }
+        public decimal PointsPossible { get; set; }
+        public decimal Average {get;set;}
+        public string LetterGrade {get;set;}
+
+        public void CalculateAverage()
+        {
+            try
+            {
+                this.Average = (this.PointsEarned / this.PointsPossible) * 100;
+            }
+            catch
+            {
+                System.Console.WriteLine("Division by zero is not possible");
+            }
+
+        }
+
+        public void DetermineLetterGrade()
+        {
+            if (this.Average >= 90)
+            {
+                this.LetterGrade = "A";
+            }
+            else if (this.Average >= 80)
+            {
+                this.LetterGrade = "B";
+            }
+            else if (this.Average >= 70)
+            {
+                this.LetterGrade = "C";
+            }
+            else if (this.Average >= 60)
+            {
+                this.LetterGrade = "D";
+            }
+            else
+            {
+                this.LetterGrade = "F";
+            }
+        }
+    }
+}
